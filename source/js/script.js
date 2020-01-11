df9c65feadbc0a75708020aa3c2fc70696cbaa35
silentPartner <hytornoy-99@mail.ru>
2020-01-11 03:55:45 +0300

    Почти доделал index

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,7 @@ var burger = document.querySelector('.header__burger');
 var header = document.querySelector('.header');
 var list = document.querySelector('.header__nav');
 var headerLogoWrap = document.querySelector('.header__logo-wrap');
+var greeting = document.querySelector('.greeting');
 
 burger.addEventListener('click', function(evt) {
   evt.preventDefault();
@@ -9,4 +10,5 @@ burger.addEventListener('click', function(evt) {
   header.classList.toggle('header--opened');
   list.classList.toggle('nav--opened');
   headerLogoWrap.classList.toggle('header__logo-wrap--opened');
+  greeting.classList.toggle('greeting--opened');
 });
